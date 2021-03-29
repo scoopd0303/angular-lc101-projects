e84d57b6eda90d249cb0a896f2a457f3ade1803f
Scott McDonald <scottmcdonald@Scotts-MacBook-Air.local>
2021-03-29 11:13:38 -0500

    finished part 1 of exercise

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'Here are some photos of cute pit bulls';
+  image1 = 'https://www.aspcapetinsurance.com/media/2001/facts-about-pit-bulls.jpg';
+  image2 = 'https://i2.wp.com/www.usmagazine.com/wp-content/uploads/pitbull-48f3d963-e89f-45e1-88f4-8a25a95de489.jpg?crop=248px%2C165px%2C1305px%2C685px&resize=1200%2C630&ssl=1&quality=86&strip=all';
+  image3 = 'https://parade.com/wp-content/uploads/2018/10/pit-bull-awareness-day-ftr.jpg';
 
   constructor() { }
 
