46cfbac2c6c58e124f097012a59983a276169dd9
Scott McDonald <scottmcdonald@Scotts-MacBook-Air.local>
2021-03-30 12:35:44 -0500

    finished reading through 29.4

--- lesson2/examples/ngif-practice/src/app/chores/chores.component.ts
+++ lesson2/examples/ngif-practice/src/app/chores/chores.component.ts
@@ -6,8 +6,9 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./chores.component.css']
 })
 export class ChoresComponent implements OnInit {
-   chores = ['Empty dishwasher', 'Complete LaunchCode prep work', 'Buy groceries'];
-   finishedChores = [];
+   chores = [];
+   finishedChores = ['Empty dishwasher', 'Buy groceries', 'Complete LaunchCode prep work', 'clean bathroom'];
+
 
    targetImage = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
 
